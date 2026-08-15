c245afead08f80d73a3bc836c12a8484cb852ff6
Logan Oden <150753799+loganoden@users.noreply.github.com>
2026-08-15 14:14:44 -0400

    Add: meeting-schedule-ii - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-2.py
@@ -0,0 +1,30 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        if not intervals:
+            return 0
+
+        # Sort by start time
+        intervals.sort(key=lambda x: x.start)
+        req_rooms = 1   
+
+        # Create data structure containing end times of meetings currently occupying rooms
+        current_ends = [intervals[0].end]
+
+        for i in range(1, len(intervals)):
+            # If a room has emptied, then we don't need a new one
+            if intervals[i].start > min(current_ends):
+                current_ends.remove(min(current_ends))
+            # Otherwise, we need a new room
+            else:
+                req_rooms += 1
+            current_ends.append(intervals[i].end)   
+
+        return req_rooms
