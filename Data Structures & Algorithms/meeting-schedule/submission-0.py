181f31664cf1d6b45449e36d7e59211ffa57dd5b
Logan Oden <150753799+loganoden@users.noreply.github.com>
2026-08-14 23:06:34 -0400

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.py
@@ -0,0 +1,21 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        # Sort intervals based on starting interval value
+        intervals.sort(key=lambda x: x.start)
+
+        prev = intervals[0]
+
+        for i in range(1, len(intervals)):
+            prev = intervals[i-1]
+            if intervals[i].start <= prev.end:
+                return False
+        
+        return True
