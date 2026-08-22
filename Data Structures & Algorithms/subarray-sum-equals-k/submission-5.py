848a8549380ea21f46198a2bb1f797d0eff2d1f6
Logan Oden <150753799+loganoden@users.noreply.github.com>
2026-08-22 13:42:17 -0400

    Add: subarray-sum-equals-k - submission-5

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-5.py
@@ -0,0 +1,19 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        total_count = 0
+        current_sum = 0
+
+        # Prefix Sum : Occurences
+        prefix = {0: 1}
+
+        for num in nums:
+            current_sum += num
+
+            needed = current_sum - k
+
+            if needed in prefix:
+                total_count += prefix[needed]
+
+            prefix[current_sum] = prefix.get(current_sum, 0) + 1
+        
+        return total_count
